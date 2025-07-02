0e3914f14a6b350b66b887fc42d15068ff15b9b7
gustavo <gustavo.gonzalez8190@gmail.com>
2025-07-02 00:07:18 -0400

    Servidor Intermedio: Función para enviar data json al sv final (no testeada)

--- Servidor_Intermedio/forward.py
+++ Servidor_Intermedio/forward.py
@@ -1,3 +1,10 @@
-def forward_data():
-    
-    return 
+import socket
+from config import FINAL_SERVER_HOST, FINAL_SERVER_PORT
+
+def forward_data(data_json):
+
+    data_bytes = data_json.encode('utf-8')
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((FINAL_SERVER_HOST, FINAL_SERVER_PORT))
+        s.sendall(data_bytes)
