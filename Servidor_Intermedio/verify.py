7d27354f1cf1d0d9b4a8dc2845f5d747545e06f3
gustavo <gustavo.gonzalez8190@gmail.com>
2025-07-01 23:53:37 -0400

    Servidor Intermedio: Función para verificar data bin (no testeada)

--- Servidor_Intermedio/verify.py
+++ Servidor_Intermedio/verify.py
@@ -1,3 +1,27 @@
-def verify_data():
+import struct
+from config import EXPECTED_SIZE
+
+def verify_data(data_bin):
+
+    if len(data_bin) != EXPECTED_SIZE:
+        return False
+
+    try:
+        unpacked = struct.unpack('<hQfffI', data_bin)
+    except struct.error as e:
+        return False
+
+    id_sensor = unpacked[0]
+    timestamp = unpacked[1]
+    temperatura = unpacked[2]
+    presion = unpacked[3]
+    humedad = unpacked[4]
+    checksum = unpacked[5]
+
+    data_without_checksum = data_bin[0:-4]
+    checksum_calculation = sum(data_without_checksum)
+
+    if checksum_calculation != checksum:
+        return False
     
-    return 
+    return True
