92e08dc527114364badeacb11414c9c3549737c3
gustavo <gustavo.gonzalez8190@gmail.com>
2025-07-01 23:02:02 -0400

    Servidor Intermedio: Función para pasar de bin a json (no testeada)

--- Servidor_Intermedio/bin_to_json.py
+++ Servidor_Intermedio/bin_to_json.py
@@ -1,3 +1,26 @@
-def bin_to_json():
-    
-    return 
+import struct
+import json
+
+def bin_to_json(data_bin):
+
+    unpacked = struct.unpack('<hQfffI', data_bin)
+
+    id_sensor = unpacked[0]
+    timestamp = unpacked[1]
+    temperatura = unpacked[2]
+    presion = unpacked[3]
+    humedad = unpacked[4]
+    checksum = unpacked[5]
+
+    datos = {
+        "id": id_sensor,
+        "timestamp": timestamp,
+        "temperatura": temperatura,
+        "presion": presion,
+        "humedad": humedad,
+        "checksum": checksum
+    }
+
+    data_json = json.dumps(datos)
+
+    return data_json
